056b045408250e38628b1f69643dfb25216393b0
Sergio Visinoni <piffio@piffio.org>
2014-07-08 23:00:09 -0400

    Add skeleton worker
    This is mostly there for validating the new diretory
    structure.

---
+++ worker/worker.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"code.google.com/p/getopt"
+)
+
+func main() {
+	// Parse cmdline args
+	cmd_help := getopt.BoolLong("help", 'h', "", "Show command help")
+	cmd_cfg := getopt.StringLong("config", 'c', "owmapi.json", "Config file")
+
+	var opts = getopt.CommandLine
+	opts.Parse(os.Args)
+
+	if *cmd_help {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	fmt.Println("Got config file ", *cmd_cfg)
+}
